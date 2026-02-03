04864d7f2d8ff7df6831718b17711f4289b42edc
Praise Jacob <iampraisejacob@gmail.com>
2026-02-04 03:17:51 +0530

    feat(cli): add command for creating new fx and updating env

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,12 +1,12 @@
 import { Command } from "commander";
-import { createGroup } from "./methods";
+import { createFx, createGroup, setEnv } from "@/cli/methods";
 
 const program = new Command();
 
 program
     .name("svrfx")
     .description("svrfx cli to create and manage functions")
-    .version("0.0.0");
+    .version("0.0.0", "-v, --version", "output the current version");
 
 // svrfx create-group <name>
 program
@@ -15,5 +15,14 @@ program
     .description("Create a new function group")
     .argument("<name>", "Name of the function group")
     .action(createGroup);
+
+program
+    .command("env")
+    .description("Create or change environment variables")
+    .argument("<key>", "Environment variable key")
+    .argument("<value>", "Environment variable value")
+    .action(setEnv);
+
+program.command("create").description("Create a new function").action(createFx);
 
 program.parse(process.argv);
--- src/cli/methods.ts
+++ src/cli/methods.ts
@@ -1,4 +1,6 @@
+import { confirm, input, select } from "@inquirer/prompts";
 import chalk from "chalk";
+import logSymbols from "log-symbols";
 import ora from "ora";
 import { FxGroup } from "@/class/FxGroup";
 
@@ -9,11 +11,138 @@ export async function createGroup(name: string) {
         const group = await FxGroup.create(process.cwd(), name);
         await group.save();
     } catch (error: unknown) {
-        spinner.fail(
-            `Failed to create group: ${chalk.red.bold((error as Error).message)}`,
-        );
+        spinner.fail(`${chalk.red.bold((error as Error).message)}`).stop();
         process.exit(1);
     }
 
-    spinner.succeed(`Group ${chalk.cyan.bold(name)} created successfully`);
+    spinner
+        .succeed(`Group ${chalk.cyan.bold(name)} created successfully`)
+        .stop();
+}
+
+export async function setEnv(key: string, value: string) {
+    const spinner = ora(`Loading Group`).start();
+    try {
+        const group = await FxGroup.load(process.cwd());
+        group.setEnv(key, value);
+        await group.save();
+    } catch (error: unknown) {
+        spinner.fail(`${chalk.red.bold((error as Error).message)}`).stop();
+        process.exit(1);
+    }
+
+    spinner
+        .succeed(
+            `Environment variable ${chalk.cyan.bold(key)} set successfully`,
+        )
+        .stop();
+}
+
+export async function listFx() {}
+
+export async function deleteFx() {}
+
+export async function createFx() {
+    const spinner = ora("Loading group").start();
+    let group: FxGroup;
+
+    try {
+        group = await FxGroup.load(process.cwd());
+    } catch (error: unknown) {
+        spinner.fail(`${chalk.red.bold((error as Error).message)}`).stop();
+        process.exit(1);
+    }
+
+    spinner.succeed("Group loaded successfully").stop();
+
+    const name = await input({
+        message: "Function name",
+        required: true,
+    });
+
+    const route = await input({
+        message: "Function route",
+        default: `/${name}`,
+        required: true,
+    });
+
+    const method = await select({
+        message: "Function method",
+        choices: [
+            {
+                name: "GET",
+                value: "get",
+            },
+            {
+                name: "POST",
+                value: "post",
+            },
+        ],
+    });
+
+    const sandbox = await select({
+        message: "Sandboxing level",
+        choices: [
+            {
+                name: "Disable",
+                value: 0,
+                description: "Directly loaded using require",
+            },
+            {
+                name: "Service Workers",
+                value: 1,
+                description: "Loaded using service workers",
+            },
+            {
+                name: "Child Process",
+                value: 2,
+                description: "Loaded using child processes",
+            },
+            {
+                name: "Containers (Not implemented)",
+                value: 3,
+                description: "Loaded using containers",
+            },
+        ],
+    });
+
+    const existing = group.fx.find(
+        (fx) => fx.name === name && fx.route === route && fx.method === method,
+    );
+    if (existing) {
+        console.log(
+            logSymbols.warning,
+            chalk.yellow.bold(
+                ` Function with the same name, route and method already exists.`,
+            ),
+        );
+
+        const proceed = await confirm({
+            message: "Do you want to proceed anyway?",
+            default: false,
+        });
+
+        if (!proceed) {
+            console.log(chalk.red.bold("Aborting function creation."));
+            process.exit(0);
+        }
+    }
+
+    spinner.text = `Creating function ${chalk.cyan.bold(name)}`;
+
+    try {
+        group.addFx({
+            name,
+            route,
+            method: method as "get" | "post",
+            sandbox: sandbox as 0 | 1 | 2 | 3,
+        });
+        await group.save();
+        spinner
+            .succeed(`Function ${chalk.cyan.bold(name)} created successfully`)
+            .stop();
+    } catch (error: unknown) {
+        spinner.fail(`${chalk.red.bold((error as Error).message)}`).stop();
+        process.exit(1);
+    }
 }
